1ca7725fb26900d45b09955ca09a92fe70b0fe71
Emir demir <emirdemir@Emirs-MacBook-Air.local>
2023-03-24 15:42:55 +0300

    Initial commit

---
+++ src/MahkemeUcreti.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MahkemeUcreti {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Juri sayısı: ");
+        int juriSayisi = input.nextInt();
+
+        int birinciHakimUcreti = 3750;
+        int ikinciHakimUcreti = 3200;
+        int juriUcreti = 50;
+
+        double beraatOrani = 0;
+        int toplamUcret = 0;
+
+        // tüm jüriler suçsuz karar verirse
+        beraatOrani = 1;
+        toplamUcret = juriSayisi * juriUcreti;
+        System.out.println("Tüm jüriler suçsuz karar verirse:");
+        System.out.println("Beraat oranı: %" + beraatOrani*100);
+        System.out.println("Toplam ücret: " + toplamUcret);
+
+        // yarısı suçsuz, hakim kararsız kalırsa
+        beraatOrani = 0.5;
+        toplamUcret = juriSayisi/2 * juriUcreti + birinciHakimUcreti/2 + ikinciHakimUcreti/2;
+        System.out.println("Yarısı suçsuz, hakim kararsız kalırsa:");
+        System.out.println("Beraat oranı: %" + beraatOrani*100);
+        System.out.println("Toplam ücret: " + toplamUcret);
+
+        // hakim doğrudan suçsuz derse
+        beraatOrani = 1;
+        toplamUcret = birinciHakimUcreti + ikinciHakimUcreti;
+        System.out.println("Hakim doğrudan suçsuz derse:");
+        System.out.println("Beraat oranı: %" + beraatOrani*100);
+        System.out.println("Toplam ücret: " + toplamUcret);
+    }
+}
+
