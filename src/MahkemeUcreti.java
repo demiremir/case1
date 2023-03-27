568e4c788f5fa8c6cf3cf4189cc042a95d615c91
emirdemir <d.eemirrr@gmail.com>
2023-03-27 09:58:14 +0300

    Last update

--- src/MahkemeUcreti.java
+++ src/MahkemeUcreti.java
@@ -22,17 +22,26 @@ public class MahkemeUcreti {
 
         // yarısı suçsuz, hakim kararsız kalırsa
         beraatOrani = 0.5;
-        toplamUcret = juriSayisi/2 * juriUcreti + birinciHakimUcreti/2 + ikinciHakimUcreti/2;
-        System.out.println("Yarısı suçsuz, hakim kararsız kalırsa:");
+        if (birinciHakimUcreti < ikinciHakimUcreti) {
+            toplamUcret = juriSayisi/2 * juriUcreti + birinciHakimUcreti/2;
+            System.out.println("Yarısı suçsuz, birinci hakim kararsız kalırsa:");
+        } else {
+            toplamUcret = juriSayisi/2 * juriUcreti + ikinciHakimUcreti/2;
+            System.out.println("Yarısı suçsuz, ikinci hakim kararsız kalırsa:");
+        }
         System.out.println("Beraat oranı: %" + beraatOrani*100);
         System.out.println("Toplam ücret: " + toplamUcret);
 
         // hakim doğrudan suçsuz derse
         beraatOrani = 1;
-        toplamUcret = birinciHakimUcreti + ikinciHakimUcreti;
-        System.out.println("Hakim doğrudan suçsuz derse:");
+        if (birinciHakimUcreti < ikinciHakimUcreti) {
+            toplamUcret = birinciHakimUcreti;
+            System.out.println("Hakim doğrudan suçsuz derse (birinci hakim):");
+        } else {
+            toplamUcret = ikinciHakimUcreti;
+            System.out.println("Hakim doğrudan suçsuz derse (ikinci hakim):");
+        }
         System.out.println("Beraat oranı: %" + beraatOrani*100);
         System.out.println("Toplam ücret: " + toplamUcret);
     }
 }
-
